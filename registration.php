ced1dcc70f3d2babb84a841e60535b95c4869175
Your Name <mmdhudasia@gmail.com>
2020-07-30 10:39:13 +0530

    Initial release

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+ /**
+  * Solwin Infotech
+  * Solwin AjaxCart Extension
+  *
+  * @category   Solwin
+  * @package    Solwin_AjaxCart
+  * @copyright  Copyright © 2006-2017 Solwin (https://www.solwininfotech.com)
+  * @license    https://www.solwininfotech.com/magento-extension-license/
+  */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_AjaxCart',
+    __DIR__
+);
